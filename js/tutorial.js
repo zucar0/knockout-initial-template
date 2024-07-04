f7a274e1b220280e05e21b52432440b674399c22
Antonio Enríquez <42826452+zucar0@users.noreply.github.com>
2024-07-04 16:31:40 +0000

    Adding title properti to viewmodel

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,6 +1,7 @@
 console.log('Tutorial');
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
+    this.title = ko.observable('Tutorial: learn.knockoutjs.com');
     this.firstName = "Bert";
     this.lastName = "Bertington";
 }
